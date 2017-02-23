0b05eaec832cf12b4fd2f18b76bba9943395ac19
unknown <>
2017-02-23 22:33:35 +0800

    早上就debug成功了，现在来提交一下代码——实现针对一组不同元素的快速排序

--- algorithms/src/main/java/com/nice/algorithms/main/QuickSortMain.java
+++ algorithms/src/main/java/com/nice/algorithms/main/QuickSortMain.java
@@ -2,14 +2,20 @@ package com.nice.algorithms.main;
 
 import com.nice.algorithms.method.QuickSort;
 
+/**
+ * 快排实现从左到右从小到大排序，每一次排序所选取的middle为最右边元素（数组最后一个元素）
+ * @author NiceWang
+ *
+ */
 public class QuickSortMain 
 {
     public static void main( String[] args )
     {
     	QuickSort quickSort = new QuickSort();
     	int length;
-        int[] input = {1,4,2,3,9,7,8};
-//    	int[] input = {6,4,2,5,3,7,8,0,1,9};
+//      int[] input = {1,4,2,3,9,7,8};  //一般用例
+//    	int[] input = {6,4,2,5,3,7,8,0,1,9}; //特殊情况用例一：除middle外所有数都比middle小
+        int[] input = {1,4,2,3,9,7,0};  //一般用例二：除middle外所有数都比middle小
         length = input.length;
         int right = length - 2;
         quickSort.Sort(input, 0, right);
--- algorithms/src/main/java/com/nice/algorithms/method/QuickSort.java
+++ algorithms/src/main/java/com/nice/algorithms/method/QuickSort.java
@@ -1,9 +1,20 @@
 package com.nice.algorithms.method;
 
+/**
+ * 快排实现从左到右从小到大排序，每一次排序所选取的middle为最右边元素（数组最后一个元素）
+ * @author NiceWang
+ *
+ */
 public class QuickSort {
-
+	/**
+	 * 针对的场景：所要排序的一组元素中不包含相同元素
+	 * @param input
+	 * @param left
+	 * @param right
+	 */
 	public void Sort(int input[], int left, int right) {
 		int temp;
+		//针对只有两个元素的情况
 		if(right - left == 0) {
 			if(input[left] > input[right + 1]) {
 				temp = input[left];
@@ -12,25 +23,32 @@ public class QuickSort {
 			}
 			return;
 		}
+		//针对只有一个元素的情况
 		if(right - left < 0) {
 			return;
 		}
     	int flag = input[right + 1];
     	int i = left, j = right;
-    	while( i < j ) {
+    	while( i < j ) { //左指针和右指针还没有交叉时
+    		//移动左指针
     		int a = input[i];
     		while(a < flag && i <= right){
     			i++;
-    			a = input[i];
+    			if(i <= right) {
+    				a = input[i];
+    			}
     		}
     		if(i > right) {
     			Sort(input, left, right-1);
     			return;
     		}
+    		//移动右指针
     		int b = input[j];
     		while(b > flag && j >= left){
     			j--;
-    			b = input[j];
+    			if(j >= left) {
+    				b = input[j];
+    			}
     		}
     		if(j < left) {
     			temp = input[left];
@@ -39,17 +57,23 @@ public class QuickSort {
     			Sort(input, left + 1, right);
     			return;
     		}
-    		temp = input[i];
-    		input[i] = input[j];
-    		input[j] = temp;
-    		i++;
-    		j--;
+    		//交换左指针所指位置元素和右指针所指位置元素，仅当左右指针还没有交叉时交换才有意义
+    		if(i < j) { //这句判断很重要啊，不然就有bug！！！
+    			temp = input[i];
+        		input[i] = input[j];
+        		input[j] = temp;
+        		i++; //这两句话有没有对时间复杂度有何影响？
+        		j--;
+    		}
     	}
+    	//交换左指针所指元素和middle
     	temp = input[i];
     	input[i] = input[right + 1];
     	input[right + 1] = temp;
+    	//递归的方式，对middle左边进行快排
     	int left_L = left, right_L = i - 2;
     	Sort(input, left_L, right_L);
+    	//对middle右边进行快排
     	int left_R = i + 1, right_R = right;
     	Sort(input, left_R, right_R);
     }
