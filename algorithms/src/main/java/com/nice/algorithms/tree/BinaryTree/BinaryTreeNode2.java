8084defadf4c0b80eabc2eed523fdfc9af96a43e
王晓楠 <wangxiaonan@wangxiaonandeMacBook-Pro.local>
2017-08-27 18:19:22 +0800

    树及二叉树结构描述

---
+++ algorithms/src/main/java/com/nice/algorithms/tree/BinaryTree/BinaryTreeNode2.java
@@ -0,0 +1,21 @@
+package com.nice.algorithms.tree.BinaryTree;
+
+public class BinaryTreeNode2 {
+
+	private int leftChild;
+	private int rightChild;
+	
+	public int getLeftChild() {
+		return leftChild;
+	}
+	public void setLeftChild(int leftChild) {
+		this.leftChild = leftChild;
+	}
+	public int getRightChild() {
+		return rightChild;
+	}
+	public void setRightChild(int rightChild) {
+		this.rightChild = rightChild;
+	}
+	
+}
---
+++ algorithms/src/main/java/com/nice/algorithms/tree/TreeNode2.java
@@ -0,0 +1,28 @@
+package com.nice.algorithms.tree;
+
+public class TreeNode2 {
+
+	private int data;
+	private int parent;
+	private int children;
+	
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public int getParent() {
+		return parent;
+	}
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+	public int getChildren() {
+		return children;
+	}
+	public void setChildren(int children) {
+		this.children = children;
+	}
+	
+}
